73ae31dc339ca04a9d53caaec070bf5dc34cd5a2
aditi9012 <aditi.1721it1011@kiet.edu>
2020-11-27 19:59:43 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ export default function App() {
     <div>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
+      <button>Submit</button>
     </div>
   );
 }
